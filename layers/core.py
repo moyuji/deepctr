921e139cb772c7df82f33fc38b6b09dd9c4bea5b
Yuji Mo <ymo@ailaw.ai>
2022-01-01 23:57:55 -0800

    sdm debug

--- layers/core.py
+++ layers/core.py
@@ -150,7 +150,7 @@ class SparseEncoding(nn.Module):
         alpha = alpha0 + self.norm_weight
         weight = self.sample_attention(alpha)
         if random.random() < 0.01:
-            print(f'{self.norm_weight} alpha0 {np.mean(alpha0.cpu().detach().numpy()) alpha {np.mean(alpha.cpu().detach().numpy())} weight {np.mean(weight.cpu().detach().numpy())}')
+            print(f'{self.norm_weight} alpha0 {np.mean(alpha0.cpu().detach().numpy())} alpha {np.mean(alpha.cpu().detach().numpy())} weight {np.mean(weight.cpu().detach().numpy())}')
         embedding = embedding * weight
         return embedding
 
